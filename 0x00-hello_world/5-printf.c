cfe6922586bfd21e379b52608518bc88ecc3d986
Tshegofatso Tswai <tshegofatso.p.tswai@gmail.com>
2022-06-30 21:40:12 -0700

    updated the print f c program

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -4,9 +4,10 @@
 /**
  * main - Entry point
  *
- * Returnn: Always 0 (Success)
+ * Return: Always 0 (Success)
  */
-int main(void){
+int main(void)
+{
 	printf("with proper grammer, but the outcome is a piece of art,\n");
 	return (0);
 }
