9889301cd6f26ecd1ecdda77115eec54275a96c2
Tshegofatso Tswai <tshegofatso.p.tswai@gmail.com>
2022-08-01 16:36:09 -0700

    Created a function-like macro ABS(x) that computes the absolute value of a number x.

--- 0x0D-preprocessor/3-function_like_macro.h
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -1,6 +1,4 @@
-#ifndef FUNCTION_LIKE_MACRO_H
-#define FUNCTION_LIKE_MACRO_H
-
-#define ABS(x) ((x) < 0 ? (-x) : (x))
-
-#endif
+#ifndef ABS_DEF
+#define ABS_DEF
+#define ABS(x) (x > 0 ? (x) : (x) * -1)
+#endif /* end ABS_DEF */
