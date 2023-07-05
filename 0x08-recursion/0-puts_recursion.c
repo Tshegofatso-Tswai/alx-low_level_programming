2e3d9a44b4a2efbd2b13cdf3bfb47767ff1a9354
Tshegofatso Tswai <pholosotswai@gmail.com>
2023-07-05 14:16:58 -0700

    This checks if *s is not null, prints the character, and recursively calls itself with the incremented pointer, otherwise it prints a new line character and returns.

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -7,15 +7,11 @@
  */
 void _puts_recursion(char *s)
 {
-    if (*s)
-    {
-        putchar(*s);
-        _puts_recursion(s + 1);
-    }
-    else
-    {
-        putchar('\n');
-        return;
-    }
+	if (*s != '\0')
+	{
+		_putchar(*s);
+		_puts_recursion(s + 1);
+	}
+	else
+		_putchar('\n');
 }
-
