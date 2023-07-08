779319d1afe4496044a2761cf4f324dbde927474
Tshegofatso Tswai <pholosotswai@gmail.com>
2023-07-08 02:33:30 -0700

    In this code, there are two functions. The sqrt_helper and the main function _sqrt_recursion. The _sqrt_recursion function serves as a wrapper function that calls the sqrt_helper function and passes the initial values for n and i.

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * _sqrt_recursion - returns the natural square root of a number.
+ * @n: number
+ *
+ * Return: natural square root of n, or -1 if it doesn't have one.
+ */
+int _sqrt_recursion(int n)
+{
+	return (sqrt_helper(n, 1));
+}
+
+/**
+ * sqrt_helper - helper function for calculating square root recursively.
+ * @n: number
+ * @i: integer to check for square root
+ *
+ * Return: natural square root of n, or -1 if it doesn't have one.
+ */
+int sqrt_helper(int n, int i)
+{
+	if (i * i == n)
+		return (i);
+	else if (i * i > n)
+		return (-1);
+	else
+		return (sqrt_helper(n, i + 1));
+}
+
