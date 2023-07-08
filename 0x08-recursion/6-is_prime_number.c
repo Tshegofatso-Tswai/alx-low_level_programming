f33c3f19ad73036a14963c1c432ae3ee23d440a7
Tshegofatso Tswai <pholosotswai@gmail.com>
2023-07-08 02:50:22 -0700

    In this code, there are two functions. The is_prime_number function and the is_prime_helper function. The is_prime_helper function is a recursive helper function that checks if a number n is divisible by any number from 2 up to the square root of n.

---
+++ 0x08-recursion/6-is_prime_number.c
@@ -0,0 +1,33 @@
+#include "main.h"
+
+/**
+ * is_prime_number - checks if a number is prime.
+ * @n: number
+ *
+ * Return: 1 if prime, 0 otherwise.
+ */
+int is_prime_number(int n)
+{
+	if (n <= 1)
+		return (0);
+
+	return (is_prime_helper(n, 2));
+}
+
+/**
+ * is_prime_helper - helper function to check if a number is prime recursively.
+ * @n: number
+ * @div: divisor
+ *
+ * Return: 1 if prime, 0 otherwise.
+ */
+int is_prime_helper(int n, int div)
+{
+	if (n % div == 0)
+		return (0);
+	else if (div * div > n)
+		return (1);
+	else
+		return (is_prime_helper(n, div + 1));
+}
+
