99b5edc37b913508ce41c739d9d83ae2697335c9
Tshegofatso Tswai <pholosotswai@gmail.com>
2023-07-10 10:36:07 -0700

    Done

--- 0x09-static_libraries/_puts.c
+++ 0x09-static_libraries/_puts.c
@@ -1,13 +1,20 @@
 #include "main.h"
-#include <stdio.h>
 
+/**
+ * _puts - prints a string, followed by a new line, to stdout
+ * @str: pointer to the string to be printed
+ *
+ * Return: void
+ */
 void _puts(char *str)
 {
-	while (*str != '\0')
+	int i = 0;
+
+	while (str[i] != '\0')
 	{
-		putchar(*str);
-		str++;
+		_putchar(str[i]);
+		i++;
 	}
-	putchar('\n');
+	_putchar('\n');
 }
 
