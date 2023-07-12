8da359604c4fbb913e076c7fd968586092bb153d
Tshegofatso Tswai <pholosotswai@gmail.com>
2023-07-12 03:21:02 -0700

    Done

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,6 +1,14 @@
 #include <stdio.h>
 
-int main(int argc, char *argv[]) {
+/**
+ * main - Entry point
+ * @argc: Number of command-line arguments
+ * @argv: Array of command-line arguments
+ *
+ * Return: 0 (Success)
+ */
+int main(int argc, char *argv[])
+{
 	printf("%s\n", argv[0]);
 	return 0;
 }
