5d879fb8defe212528fc8627200d1a554f795546
Tshegofatso Tswai <pholosotswai@gmail.com>
2023-07-12 10:36:49 -0700

    Done

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -1,5 +1,4 @@
 #include "main.h"
-#include <string.h>
 
 /**
  * _strdup - Returns a pointer to a newly allocated space in memory,
--- 0x0B-malloc_free/main.h
+++ 0x0B-malloc_free/main.h
@@ -3,6 +3,7 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 /* Function prototypes */
 char *create_array(unsigned int size, char c);
