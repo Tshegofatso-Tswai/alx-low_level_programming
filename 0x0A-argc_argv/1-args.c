c7ee3aff872b844fea7cedb532fcd1a3acc90f78
Tshegofatso Tswai <pholosotswai@gmail.com>
2023-07-12 03:36:35 -0700

    Done

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * @argc: Number of command-line arguments
+ * @argv: Array of command-line arguments
+ *
+ * Return: 0 (Success)
+ */
+int main(int argc, char *argv[])
+{
+	printf("%d\n", argc - 1);
+	return 0;
+}
+
