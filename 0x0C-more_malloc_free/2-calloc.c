3300a6ee2bd495799672701313308d6f1eaec092
Tshegofatso Tswai <pholosotswai@gmail.com>
2023-07-16 16:55:23 -0700

    The _calloc function allocates memory for an array, initializes the allocated memory to zero, and returns a pointer to the allocated memory block.

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -6,25 +6,24 @@
  * @size: size of bytes.
  *
  * Return: pointer to the allocated memory.
- *         If nmemb or size is 0, returns NULL.
- *         If malloc fails, returns NULL.
+ * if nmemb or size is 0, returns NULL.
+ * if malloc fails, returns NULL.
  */
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
+	char *p;
+	unsigned int i;
+
 	if (nmemb == 0 || size == 0)
 		return (NULL);
 
-	void *ptr = malloc(nmemb * size);
+	p = malloc(nmemb * size);
 
-	if (ptr == NULL)
+	if (p == NULL)
 		return (NULL);
-
-	char *p = ptr;
-	unsigned int i;
 
 	for (i = 0; i < (nmemb * size); i++)
 		p[i] = 0;
 
-	return (ptr);
+	return (p);
 }
-
