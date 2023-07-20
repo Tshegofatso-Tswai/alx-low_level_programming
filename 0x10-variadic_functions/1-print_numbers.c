73fabc4839dc7873f8bd3014ca54695695a37f1f
Tshegofatso Tswai <pholosotswai@gmail.com>
2023-07-20 16:26:30 -0700

    The print_numbers function takes a string separator and an unsigned integer n as its parameters.

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -1,30 +1,27 @@
 #include "variadic_functions.h"
-#include <stdio.h>
-#include <stdarg.h>
 
 /**
- * print_numbers - Prints numbers followed by a new line.
- * @separator: The string to be printed between numbers.
- * @n: The number of integers passed to the function.
+ * print_numbers - prints numbers.
+ * @separator: string to be printed between numbers.
+ * @n: number of integers passed to the function.
  *
- * Return: Nothing.
+ * Return: no return.
  */
 void print_numbers(const char *separator, const unsigned int n, ...)
 {
-	va_list args;
+	va_list valist;
 	unsigned int i;
 
-	va_start(args, n);
+	va_start(valist, n);
 
 	for (i = 0; i < n; i++)
 	{
-		printf("%d", va_arg(args, int));
-
-		if (separator != NULL && i < n - 1)
+		printf("%d", va_arg(valist, int));
+		if (separator && i < n - 1)
 			printf("%s", separator);
 	}
 
 	printf("\n");
-	va_end(args);
+	va_end(valist);
 }
 
