ca289e562730bb459669bbe1ef34e7794e634a8c
Tshegofatso Tswai <pholosotswai@gmail.com>
2023-07-20 16:15:39 -0700

    functions

---
+++ 0x10-variadic_functions/variadic_functions.h
@@ -0,0 +1,29 @@
+#ifndef VARIADIC_FUNCTIONS_H
+#define VARIADIC_FUNCTIONS_H
+
+#include <stdarg.h>
+
+/**
+ * struct format_specifier - Struct format_specifier
+ * @spec: The format specifier
+ * @func: The function associated
+ *
+ * Description: A structure that maps format specifiers
+ * to the corresponding functions.
+ */
+typedef struct format_specifier
+{
+	char *spec;
+	void (*func)(va_list);
+} format_specifier_t;
+
+void print_char(va_list args);
+void print_integer(va_list args);
+void print_float(va_list args);
+void print_string(va_list args);
+
+void print_all(const char * const format, ...);
+int sum_them_all(const unsigned int n, ...);
+
+#endif /* VARIADIC_FUNCTIONS_H */
+
