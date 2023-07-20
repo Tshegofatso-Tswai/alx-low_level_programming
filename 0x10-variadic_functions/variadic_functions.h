718c2dee4aea8c7c2ef4fd1fc38a8cd67de79c15
Tshegofatso <106757261+Tshegofatso-Tswai@users.noreply.github.com>
2023-07-21 01:46:50 +0200

    Updated variadic_functions.h

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -22,8 +22,10 @@ void print_integer(va_list args);
 void print_float(va_list args);
 void print_string(va_list args);
 
-void print_all(const char * const format, ...);
 int sum_them_all(const unsigned int n, ...);
+void print_numbers(const char *separator, const unsigned int n, ...);
+void print_strings(const char *separator, const unsigned int n, ...);
+void print_all(const char * const format, ...);
 
 #endif /* VARIADIC_FUNCTIONS_H */
 
