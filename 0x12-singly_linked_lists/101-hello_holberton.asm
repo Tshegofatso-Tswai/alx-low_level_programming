ce08be8ea8964091be81fc4cc23434be21b7e212
Tshegofatso Tswai <pholosotswai@gmail.com>
2023-07-29 00:39:48 -0700

    Done

--- 0x12-singly_linked_lists/101-hello_holberton.asm
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -1,29 +1,15 @@
-section .data
-    hello db "Hello, Holberton", 0
-    format db "%s", 0
-    newline db 10, 0
+	SECTION .data
+msg:	db "Hello, Holberton", 0
+fmt:	db "%s", 10, 0
 
-section .text
-    extern printf
-
-global main
+	SECTION .text
+	extern printf
+	global main
 main:
-    ; Push the format string onto the stack (argument for printf)
-    mov rdi, format
-    ; Push the address of hello onto the stack (argument for printf)
-    mov rsi, hello
-    ; Call the printf function
-    call printf
+	mov esi, msg
+	mov edi, fmt
+	mov eax, 0
+	call printf
 
-    ; Push the format string for the new line onto the stack (argument for printf)
-    mov rdi, format
-    ; Push the address of newline onto the stack (argument for printf)
-    mov rsi, newline
-    ; Call the printf function again to print the new line
-    call printf
-
-    ; Exit the program
-    mov rax, 60         ; syscall number for exit
-    xor rdi, rdi        ; status code 0
-    syscall
-
+	mov eax, 0
+	ret
