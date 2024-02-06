ca89e96b3a2f2b867b0f602213403870c60e5030
Tshegofatso Tswai <pholosotswai@gmail.com>
2024-02-05 16:59:47 -0800

    Done

---
+++ 0x1E-search_algorithms/listint/listint.h
@@ -0,0 +1,23 @@
+#ifndef LISTINT_H
+#define LISTINT_H
+
+#include <stddef.h>
+
+/**
+ * struct listint_s - singly linked list
+ *
+ * @n: Integer
+ * @index: Index of the node in the list
+ * @next: Pointer to the next node
+ *
+ * Description: singly linked list node structure
+ */
+typedef struct listint_s
+{
+    int n;
+    size_t index;
+    struct listint_s *next;
+} listint_t;
+
+#endif 
+
